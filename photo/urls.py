12ace07f39847cf86a87655e7882ee90a0290520
sofi <sofidenner@gmail.com>
2016-02-18 00:16:28 -0300

    mostrado foto en visor

--- photo/urls.py
+++ photo/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 from . import views
+from django.views.static import serve
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^visor/(?P<pk>[0-9]+)/$', views.visor, name='visor'),
+    url(r'^(?P<path>.*)$', serve, {'document_root': '/'}),
 ]
