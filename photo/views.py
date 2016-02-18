8805a8d9537172dc9b2ebce80d5686f8e4177459
sofi <sofidenner@gmail.com>
2016-02-18 20:25:41 -0300

    Funcion acomodar andando

--- photo/views.py
+++ photo/views.py
@@ -1,4 +1,4 @@
-import glob
+import glob, shutil, os
 from django.shortcuts import render, redirect, get_object_or_404
 from photo.models import Folder
 from .forms import FormFolder
@@ -36,31 +36,51 @@ def visor(request, pk):
 
     photosList = glob.glob(carpetaActual.ruta + '*.jpg')
 
+    if request.method == "POST":
+        form = FormFolder(request.POST)
+
+        if form.is_valid():
+            folder = form.save(commit=False)
+
+            if folder.ruta[-1] != '/':
+                folder.ruta = folder.ruta + '/'
+
+            if not os.path.exists(folder.ruta):
+                try:
+                    os.makedirs(folder.ruta)
+                except:
+                    return render(
+                        request,
+                        'photo/visor.html',
+                        {'carpetas': Folder.objects.filter(tipo='destino').order_by('ruta'),
+                        'form': form,
+                        'fotos': photosList,
+                        'carpeta': carpetaActual,
+                        'error': "No se puede crear la carpeta espcificada"
+                        }
+                    )
+
+            folder.tipo = 'destino'
+
+            folder.save()
+
+            shutil.move(photosList[0], folder.ruta)
+
+            photosList = glob.glob(carpetaActual.ruta + '*.jpg')
+
+    else:
+        form = FormFolder()
+
     if photosList == []:
         return render(
         request,
-        'photo/photohome.html',
-        {'error': "No hay fotos en esa carpeta!"}
+        'photo/visor.html',
+        {'error': "No hay fotos en esa carpeta!",
+        'carpeta': carpetaActual,
+        }
         )
 
     else:
-
-        if request.method == "POST":
-            form = FormFolder(request.POST)
-
-            if form.is_valid():
-                folder = form.save(commit=False)
-
-                if folder.ruta[-1] != '/':
-                    folder.ruta = folder.ruta + '/'
-
-                folder.tipo = 'destino'
-
-                folder.save()
-
-        else:
-            form = FormFolder()
-
         return render(
             request,
             'photo/visor.html',
